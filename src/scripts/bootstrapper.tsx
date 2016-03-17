e8f5f0048d2b9708c532761acadcb7920bd3c519
Martynas Bardauskas <mb@frontit.dk>
2016-03-17 17:00:07 +0200

    added react redux

--- src/scripts/bootstrapper.tsx
+++ src/scripts/bootstrapper.tsx
@@ -1,5 +1,23 @@
 import * as React from "react";
 import * as ReactDOM  from "react-dom";
+import { createStore } from 'redux'
 import HelloWorld = require('./dom');
 
-ReactDOM.render(<HelloWorld name="world" />, document.getElementById('root'));
+let reducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state
+    }
+};
+
+let store = createStore(reducer);
+
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+ReactDOM.render(<HelloWorld name={store.getState()} />, document.getElementById('root'));
