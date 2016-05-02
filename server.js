781ed173c3181aa15a03fbaeca727fc27e3fe955
Martynas Bardauskas <martynas.bardauskas@gmail.com>
2016-05-02 23:56:29 +0300

    added endpoint to list entries from mongolab

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 import express from 'express';
+import {MongoClient} from 'mongodb';
 
 let app = new express();
 
 app.use(express.static('dist'));
 
-app.listen(3000);
+let db;
+MongoClient.connect(process.env.MONGO_MONEYLOG_URL, (err, database) => {
+    if(err) throw err;
+
+    db = database;
+    app.listen(3000);
+});
+
+app.get('/money-entries', (req, res) => {
+    db.collection('money_entries').find({}).toArray((err, entries) => {
+        if(err) throw err;
+
+        res.json(entries);
+    })
+});
